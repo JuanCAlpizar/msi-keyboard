0d7d07ae18788777c3d1a8f6654b1e404ea0876a
JuanCAlpizar <juanc.alpizar@yahoo.com>
2017-01-02 17:44:12 -0600

    Compatibility changes on mode detection for GT72-x and newer models

--- examples/light-all-regions.js
+++ examples/light-all-regions.js
@@ -28,11 +28,3 @@ keyboard.color('front-right-panel', {
 	color: 'red',
 	intensity: 'med'
 });
-
-//keyboard.blink(750);
-//keyboard.blink(['left','right'], 750);
-
-//TODO: breathe is not properly working, will need a fix on setMode.js directly
-//keyboard.mode('breathe', 'green', 'red', 'yellow');
-//TODO: wave is not properly working, will need a fix on setMode.js directly
-//keyboard.mode('wave', 'blue');
